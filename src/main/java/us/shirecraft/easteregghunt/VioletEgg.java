de43d600e36a8d2a72a67996a610da2f463d2bb8
Andy Pover <andy@angel.uk>
2019-04-26 13:38:20 +0100

    Add egg variations

---
+++ src/main/java/us/shirecraft/easteregghunt/VioletEgg.java
@@ -0,0 +1,12 @@
+package us.shirecraft.easteregghunt;
+
+public class VioletEgg extends Egg {
+    public VioletEgg() {
+        super(10, 30);
+        setTexture(VOILET_EGG_TEXTURE);
+        setPlayerUuid(VIOLET_EGG_PLAYER_UUID);
+    }
+
+    private final String VOILET_EGG_TEXTURE     = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMWNjZDUzYjhkOWU4ZjQ2M2JjMWYwNmZjMWEzNjdiNjI0YzIxZWY5MzY5NWE0MDcxZjI2OWQ4YmM3NzlkIn19fQ==";
+    private final String VIOLET_EGG_PLAYER_UUID = "b84cf9d3-faf7-49f9-a25d-9040825c6153";
+}
