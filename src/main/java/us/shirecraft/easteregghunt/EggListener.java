6886f351e0b26d0ad4853048b5e994c736aba01c
Blitheness <blitheness@oneiric.co.uk>
2020-04-13 03:00:59 +0100

    Spawn a baby chicken occasionally instead of collecting an egg

--- src/main/java/us/shirecraft/easteregghunt/EggListener.java
+++ src/main/java/us/shirecraft/easteregghunt/EggListener.java
@@ -1,20 +1,20 @@
 package us.shirecraft.easteregghunt;
 
 import de.tr7zw.nbtapi.NBTItem;
-import org.bukkit.Instrument;
-import org.bukkit.Material;
-import org.bukkit.Note;
-import org.bukkit.Particle;
-import org.bukkit.entity.Player;
+import org.bukkit.*;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Random;
+
 public class EggListener implements Listener {
     public EggListener(EasterEggHunt plugin) {
         this.plugin = plugin;
+        this.random = new Random();
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
@@ -27,10 +27,23 @@ public class EggListener implements Listener {
                 NBTItem nbtItem = new NBTItem(is);
                 String eggType = nbtItem.getString("EasterEgg");
                 player.sendMessage("§6 ** You found a " + eggType + " Egg");
-                player.playNote(player.getLocation(), Instrument.XYLOPHONE, Note.sharp(1, Note.Tone.F));
-                player.spawnParticle(Particle.SPELL_INSTANT, ev.getItem().getLocation(), 5);
-                ev.getItem().remove();
-                plugin.sendToWebServer(player, eggType);
+                if(getRandom(1,100) < 6) {
+                    player.sendMessage("§a ** Oh no. The egg hatched before you could collect it!");
+                    Location eggLocation = ev.getItem().getLocation();
+                    World world = eggLocation.getWorld();
+                    world.spawnParticle(Particle.EXPLOSION_LARGE, eggLocation, 2);
+                    ev.getItem().remove();
+                    Chicken chicken = (Chicken) world.spawnEntity(eggLocation, EntityType.CHICKEN);
+                    chicken.setBaby();
+                    chicken.setTicksLived(1);
+                    world.spawnParticle(Particle.HEART, chicken.getLocation(), 2);
+                }
+                else {
+                    player.playNote(player.getLocation(), Instrument.XYLOPHONE, Note.sharp(1, Note.Tone.F));
+                    ev.getItem().getLocation().getWorld().spawnParticle(Particle.SPELL_INSTANT, ev.getItem().getLocation(), 5);
+                    ev.getItem().remove();
+                    plugin.sendToWebServer(player, eggType);
+                }
             }
             ev.setCancelled(true);
         }
@@ -44,5 +57,10 @@ public class EggListener implements Listener {
         return false;
     }
 
+    private int getRandom(int min, int max) {
+        return random.nextInt(max - min) + min;
+    }
+
     private final EasterEggHunt plugin;
+    private Random random;
 }
